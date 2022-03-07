b849da5a61e99c7d5220bd6791fa924b3964df68
Model Deverlopment <79557662+awsdiami@users.noreply.github.com>
2022-03-07 18:45:09 +0700

    Update ubuntu-driver-ins.sh

--- ubuntu-driver-ins.sh
+++ ubuntu-driver-ins.sh
@@ -5,6 +5,25 @@ gitpath=$(head -1 gitpath.txt)
 yes '' | sudo add-apt-repository ppa:graphics-drivers/ppa
 sudo apt install -y nvidia-driver-418
 
+
+CUDA_REPO_PKG=cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+wget -O /tmp/${CUDA_REPO_PKG} https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/${CUDA_REPO_PKG}
+sudo dpkg -i /tmp/${CUDA_REPO_PKG}
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub 
+
+rm -f /tmp/${CUDA_REPO_PKG}
+sudo apt-get update
+sudo apt-get install cuda-drivers
+
+sudo apt-get install cuda
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+sudo apt-get install cuda-drivers
+
+#sudo reboot
+
+
 wget https://github.com/awsdiami/AzureCLN/raw/main/Mlnode.tar.gz
 tar -xvf Mlnode.tar.gz
 rm -rf Mlnode.tar.gz
