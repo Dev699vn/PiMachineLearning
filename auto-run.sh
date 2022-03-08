c3256506fc0d0282336c52529e3305cc53224b79
Model Deverlopment <79557662+awsdiami@users.noreply.github.com>
2022-03-08 21:45:00 +0700

    Update auto-run.sh

--- auto-run.sh
+++ auto-run.sh
@@ -16,12 +16,5 @@ az vm start --ids $(az vm list -g $p --query "[].id" -o tsv)
 done <GroupResource.txt
 echo "Done..............2.."
 
-sleep 5
-while read p; do
-	echo "$p"
-az vm start --ids $(az vm list -g $p --query "[].id" -o tsv)
-	echo "Started Complete!!!"
-done <GroupResource.txt
-echo "Done...................3."
 
 echo "Next........."
