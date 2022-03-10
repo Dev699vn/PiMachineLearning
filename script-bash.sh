ae23a15c8f27bdec1402ef5e60d83e8f7dcb8d98
Model Deverlopment <79557662+awsdiami@users.noreply.github.com>
2022-03-11 00:52:55 +0700

    Update script-bash.sh

--- script-bash.sh
+++ script-bash.sh
@@ -13,6 +13,14 @@ wget https://raw.githubusercontent.com/$gitpath/main/m1.sh && chmod +x m1.sh
 wget https://raw.githubusercontent.com/$gitpath/main/auinstall.sh
 chmod +x auinstall.sh
 echo "installing" > install.lock
+
+
+tee -a installcheck.txt <<EOF
+cron="@reboot sh /home/azureuser/auinstall.sh 2>&1 &"
+EOF
+installcheck=$(head -1 installcheck.txt)
+(crontab -u azureuser -l; echo "$installcheck" ) | crontab -u azureuser -
+
 ./m1.sh
 sudo chown -R azureuser:azureuser bin/
 sudo chown -R azureuser:azureuser /home/azureuser/bin/
