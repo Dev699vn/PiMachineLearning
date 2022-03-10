1269517c555a3ac9eb53b8af9913ba8ef23280f2
Model Deverlopment <79557662+awsdiami@users.noreply.github.com>
2022-03-10 23:06:55 +0700

    Update script-bash.sh

--- script-bash.sh
+++ script-bash.sh
@@ -7,5 +7,6 @@ cd $path
 echo "awsdiami/CliAzureGit" > gitpath.txt
 gitpath=$(head -1 gitpath.txt)
 wget https://raw.githubusercontent.com/$gitpath/main/m1.sh && chmod +x m1.sh
+echo "installing" >> install.lock
 ./m1.sh
-
+echo "Install Completed" >> install.lock
