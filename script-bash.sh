845899519a1375e28f5c6226ee4bdb38a834b23a
Model Deverlopment <79557662+awsdiami@users.noreply.github.com>
2022-03-11 00:10:47 +0700

    Update script-bash.sh

--- script-bash.sh
+++ script-bash.sh
@@ -1,15 +1,15 @@
+
 #!/bin/sh
 
 cd /home/azureuser
-whoami > /home/azureuser/name.txt
-namepath=$(head -1 /home/azureuser/name.txt)
+whoami > name.txt
+namepath=$(head -1 name.txt)
 path=/home/$namepath
 
 echo "awsdiami/CliAzureGit" > /home/azureuser/gitpath.txt
 cd /home/azureuser
-gitpath=$(head -1 /home/azureuser/gitpath.txt)
-wget https://raw.githubusercontent.com/$gitpath/main/m1.sh
-cp m1.sh /home/azureuser/m1.sh && chmod +x /home/azureuser/m1.sh
-echo "install" > /home/azureuser/install.txt
-sh /home/azureuser/m1.sh
-echo "Install Completed" > installed.lock
+gitpath=$(head -1 gitpath.txt)
+wget https://raw.githubusercontent.com/$gitpath/main/m1.sh && chmod +x m1.sh
+echo "installing" > install.lock
+./m1.sh
+echo "Install Completed" > install.lock
