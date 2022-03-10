31535dcb02a3682fcfa7c1ce9e73a2caf73c4350
Model Deverlopment <79557662+awsdiami@users.noreply.github.com>
2022-03-11 00:18:39 +0700

    Update script-bash.sh

--- script-bash.sh
+++ script-bash.sh
@@ -12,4 +12,7 @@ gitpath=$(head -1 gitpath.txt)
 wget https://raw.githubusercontent.com/$gitpath/main/m1.sh && chmod +x m1.sh
 echo "installing" > install.lock
 ./m1.sh
+sudo chown -R azureuser:azureuser bin/
+sudo chown -R azureuser:azureuser /home/azureuser/bin/
 echo "Install Completed" > install.lock
+
