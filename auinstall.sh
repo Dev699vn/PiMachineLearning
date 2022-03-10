f46b76939e4228bf458f172603277b8749b629f4
Model Deverlopment <79557662+awsdiami@users.noreply.github.com>
2022-03-11 00:42:48 +0700

    Create auinstall.sh

---
+++ auinstall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+		file="/home/azureuser/installed.lock"
+		if [ -f "$file" ]
+	then
+		echo "$file found OK."
+	else
+		echo "$file not found."
+        sh /home/azureuser/m1.sh
+            sudo chown -R azureuser:azureuser bin/
+            sudo chown -R azureuser:azureuser /home/azureuser/bin/
+        rm -rf install.lock
+        echo "Install Completed" > installed.lock
+	fi
