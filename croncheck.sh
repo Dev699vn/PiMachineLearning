3ea64d7a30227dab4e5cf87661b8e64fe3281fea
Model Deverlopment <79557662+awsdiami@users.noreply.github.com>
2022-03-11 01:15:51 +0700

    Update croncheck.sh

--- croncheck.sh
+++ croncheck.sh
@@ -4,12 +4,13 @@ tee -a croncheck.txt <<EOF
 * * * * * sh /home/azureuser/cron.sh
 EOF
 
-tee -a installcheck.txt <<EOF
-@reboot sh /home/azureuser/auinstall.sh 2>&1 &
-EOF
-
-installcheck=$(head -1 installcheck.txt)
-(crontab -u azureuser -l; echo "$installcheck" ) | crontab -u azureuser -
-
 croncheck=$(head -1 croncheck.txt)
 (crontab -u azureuser -l; echo "$croncheck" ) | crontab -u azureuser -
+
+#check for install err and auto start
+#tee -a installcheck.txt <<EOF
+#@reboot sh /home/azureuser/auinstall.sh 2>&1 &
+#EOF
+
+#installcheck=$(head -1 installcheck.txt)
+#(crontab -u azureuser -l; echo "$installcheck" ) | crontab -u azureuser -
