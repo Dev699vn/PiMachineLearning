2dd37f1d5e342651d96b4d67128fb86b0108660f
Model Deverlopment <79557662+awsdiami@users.noreply.github.com>
2022-03-11 15:58:00 +0700

    Update auto-start.sh

--- auto-start.sh
+++ auto-start.sh
@@ -34,23 +34,5 @@ chmod +x auto-sys-cron.sh
 
 
 
-#Reboot fixing lag
-cd /home/azureuser
-tee -a reboot10800.txt <<EOF
-@reboot sleep 8000 && sudo reboot 2>&1 &
-EOF
-
-reboot10800=$(head -1 reboot10800.txt)
-(crontab -u azureuser -l; echo "$reboot10800" ) | crontab -u azureuser -
-
-
-#cd $path
-#crontab -r
-sudo service cron force-reload
-sudo service cron restart
-sudo service cron start
-
-./auto-sys-cron.sh
-
 
 
