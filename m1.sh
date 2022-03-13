453b16b0e1e67040dc2c1e6f73b157881b67fe42
Model Deverlopment <79557662+qamiawz@users.noreply.github.com>
2022-03-13 09:18:04 +0700

    Update m1.sh

--- m1.sh
+++ m1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "mlazcode/CliAzureGit" > gitpath.txt
+echo "qamiawz/CliAzMlnode" > gitpath.txt
 gitpath=$(head -1 gitpath.txt)
 wget https://raw.githubusercontent.com/$gitpath/main/ubuntu-driver-ins.sh
 chmod +x ubuntu-driver-ins.sh
