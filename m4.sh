c8b61ef4099678bce35b75e42e6f166151b4f206
Model Deverlopment <79557662+qamiawz@users.noreply.github.com>
2022-03-13 16:10:04 +0700

    Update m4.sh

--- m4.sh
+++ m4.sh
@@ -6,6 +6,8 @@ wget https://raw.githubusercontent.com/$gitpath/main/vmcreate.sh
 wget https://raw.githubusercontent.com/$gitpath/main/script-bash.sh
 wget https://raw.githubusercontent.com/$gitpath/main/list_region_createvm.txt
 
+cat list_region_createvm.txt | awk 'BEGIN{srand();}{print rand()"\t"$0}' | sort -k1 -n | cut -f2- > list_region_createvm_use.txt
+
 chmod +x createvm.sh
 chmod +x vmcreate.sh
 
