a59e777e60d7e0007bad0f45206a9099409ad56a
Jame <lungtungner@gmail.com>
2022-03-14 23:29:26 +0700

    Update auinstall.sh

--- auinstall.sh
+++ auinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-namepath=$(cat inuser.txt)
+namepath=$(head -1 inuser.txt)
 
 	file="/home/$namepath/installed.lock"
 		if [ -f "$file" ]
@@ -10,7 +10,7 @@ namepath=$(cat inuser.txt)
 		echo "$file not found."
         sh /home/$namepath/m1.sh
             sudo chown -R $namepath:$namepath bin/
-            sudo chown -R $namepath:$namepath /home/$namepath
+            sudo chown -R $namepath:$namepath /home/$namepath/bin
         rm -rf install.lock
         echo "Install Completed" > installed.lock
 	fi
