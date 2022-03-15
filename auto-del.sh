88d1b5cf2619a1d99b2063931df30cd59f5eb7e1
Jame <lungtungner@gmail.com>
2022-03-15 10:45:47 +0700

    Create auto-del.sh

---
+++ auto-del.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+unset DelGroupLine
+read -p "Nhap vao ten ResourceGroup can xoa: " DelGroupLine
+echo "Da xoa, Danh sach Group Hien tai con la..."
+
+sed '/$DelGroupLine/d' ./GroupResource.txt
+echo "..."
+echo "Da xoa $DelGroupLine"
