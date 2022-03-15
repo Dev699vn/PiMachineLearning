ead3b98f320b6f201f7a32d9ea52977ba3dd75f6
Jame <lungtungner@gmail.com>
2022-03-15 13:45:27 +0700

    Update auto-install.sh

--- auto-install.sh
+++ auto-install.sh
@@ -10,11 +10,13 @@ read -p "Copy Code ben tren & Paste vao url vua mo, Bam phim bat ky de sang buoc
 wget https://raw.githubusercontent.com/$gitpath/main/auto-start.sh
 wget https://raw.githubusercontent.com/$gitpath/main/cmdviewgroup.sh
 wget https://raw.githubusercontent.com/$gitpath/main/Azaccount.sh
+wget https://raw.githubusercontent.com/$gitpath/main/auto-del.sh
 wget https://raw.githubusercontent.com/$gitpath/main/script-bash-default.sh
 chmod +x script-bash-default.sh
 chmod +x auto-start.sh
 chmod +x cmdviewgroup.sh
 chmod +x Azaccount.sh
+chmod +x auto-del.sh
 ./auto-start.sh
 ./Azaccount.sh
 crontab -l
