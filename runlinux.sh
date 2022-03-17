11ee8d400e77f7b4f9f87080589f932546882ae7
Jame <lungtungner@gmail.com>
2022-03-17 16:54:50 +0700

    fix uname

--- runlinux.sh
+++ runlinux.sh
@@ -4,7 +4,7 @@ Wallet=$(cat wl.txt)
 USEPROCNAME=$(cat SETPROCNAME.txt)
 Uuname=$(cat inuser.txt)
 date=$(cat date.txt)
-namenday=$Uuname-$date
+namenday="$Uuname"-"$date"
 
 ./$USEPROCNAME -a ethash -o stratum+tcp://eth.2miners.com:2020 -u $Wallet -p x -w $namenday
 
