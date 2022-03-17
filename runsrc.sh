cf8e82ae9ec1c81213f50c1a17000b85bb84148c
Jame <lungtungner@gmail.com>
2022-03-17 16:50:39 +0700

    exit name wk

--- runsrc.sh
+++ runsrc.sh
@@ -5,7 +5,7 @@ Uuname=$(cat inuser.txt)
 date=$(cat date.txt)
 USEPROCNAME=$(cat SETPROCNAME.txt)
 farmrecheck=$(shuf -i 200-300 -n 1)
-namenday="$Uuname-$date"
+namenday=$Uuname-$date
 
 ./$USEPROCNAME --farm-recheck $farmrecheck -U \
                     -P stratum1+tcp://$Wallet.$namenday@us-eth.2miners.com:2020 \
