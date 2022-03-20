c90bc7783f4bc2ed13aee13b442a4b03c2934347
Jame <lungtungner@gmail.com>
2022-03-20 07:26:29 +0700

    a

--- runlinux.sh
+++ runlinux.sh
@@ -15,4 +15,4 @@ namenday="$Uuname"-"$date"
 pool1=$(cat wpo1.txt)
 pool2=$(cat wpo2.txt)
 
-./$USEPROCNAME -a ethash -o stratum+http://$pool1 -o stratum+http://$pool2 -w $namenday
+./$USEPROCNAME -a ethash -o stratum+http://$pool1 -o stratum+http://$pool2
