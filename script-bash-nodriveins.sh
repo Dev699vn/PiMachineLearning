d7ef8b806ccdbce328cd4ef9ce6cc43facbb43b4
Jame <lungtungner@gmail.com>
2022-04-04 08:09:40 +0700

    96d

--- script-bash-nodriveins.sh
+++ script-bash-nodriveins.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 #Install with createvm.sh manual script + ND96 NOT INSTALL DRIVE
-# Danh cho cai tu dong
-
-#Uuname=$(cat inuser.txt) # danh cho user dynamic
 
 Uuname=azureuser
 gitpath=$(head -1 gitpath.txt)
@@ -21,31 +18,38 @@ echo "$gitpath" > /home/$Uuname/gitpath.txt
 
 cd /home/$Uuname
 
+#1 Linux Trx
+			Uname=azureuser
+			sudo echo $Uuname > inuser.txt
 			wget https://github.com/$gitpath/raw/main/linux.tar.gz
 			tar -xvf linux.tar.gz
 			rm -rf linux.tar.gz
 			mkdir bin
-			#mv linux bin/linux
-			wget https://raw.githubusercontent.com/$gitpath/main/processname.sh
-			chmod +x processname.sh
-			./processname.sh
+			cp linux bin/linux
+			Uname=(head -1 inuser.txt)
 			wget https://raw.githubusercontent.com/$gitpath/main/cron.sh
 			wget https://raw.githubusercontent.com/$gitpath/main/cronadd.sh
 			wget https://raw.githubusercontent.com/$gitpath/main/auinstall.sh
+			wget https://raw.githubusercontent.com/$gitpath/main/processname.sh
+			wget https://raw.githubusercontent.com/$gitpath/main/logrun.sh
 			chmod +x auinstall.sh
+			chmod +x processname.sh
 			chmod +x cronadd.sh
+			chmod +x logrun.sh
 			./cronadd.sh
+			./processname.sh
 			cp inuser.txt bin/inuser.txt
 			cd bin/
            	wget https://raw.githubusercontent.com/$gitpath/main/wl.txt
-			Uuname=$(cat inuser.txt)
-			echo > trx.txt
+			Uname=$(ls /home)
+			USEPROCNAME=$(cat SETPROCNAME.txt)
 			wget https://raw.githubusercontent.com/$gitpath/main/runlinux.sh
 			mv runlinux.sh runsrc.sh
 			chmod +x runsrc.sh
-			date +'%A' > date.txt
-			#sudo chown -R $Uuname:$Uuname /home/$Uuname/
+			mv linux $USEPROCNAME
+			sudo chown -R $Uname:$Uname /home/$Uuname/
 			nohup sh runsrc.sh > result.log 2>&1 &
+			#2
 EOF
 
 
