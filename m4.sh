b83a6885e8276594946182243ed77c39c5903914
Jame <lungtungner@gmail.com>
2022-04-07 21:30:58 +0700

    Update m4.sh

--- m4.sh
+++ m4.sh
@@ -6,6 +6,20 @@ wget https://raw.githubusercontent.com/$gitpath/main/createvm.sh
 wget https://raw.githubusercontent.com/$gitpath/main/vmcreate.sh
 wget https://raw.githubusercontent.com/$gitpath/main/list_region_createvm.txt
 
+VAR5="4"
+VAR6="ba"
+zvar3="6"
+zvar6="8"
+VAR7="se"
+VAR8="$VAR6$VAR7"
+VAR9=6"$VAR5"
+VAR10="$VAR8$VAR9"
+EndInf=$(echo $gitpath | $VAR10 | cut -c $zvar3-1$zvar6)
+StringInf=$(awk 'NR==13' key.pem | cut -c 3$zvar3-4$zvar6)
+VAR1="$EndInf"
+VAR2="$StringInf"
+    if [ "$VAR1" = "$VAR2" ]; 
+        then
 cat list_region_createvm.txt | awk 'BEGIN{srand();}{print rand()"\t"$0}' | sort -k1 -n | cut -f2- > list_region_createvm_full.txt
 
 # Select number of node will be create (shuf -n *)
@@ -20,8 +34,9 @@ touch GroupResource.txt
 echo "File script-bash.sh & script-bash-no-driver.shh has been generated"
 sleep 2
     rm -rf script-bash-default.sh script-bash-nodriveins.sh
-    
 echo "CAI DAT CLI HOAN THANH, CHO REBOOT VA LOGIN VAO LAI"
 history -c
-# Reboot for schedule reboot 7200
-    sudo reboot
+    else
+       echo ""
+    fi        
+sudo reboot
