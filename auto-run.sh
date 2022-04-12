58e840d7a2a5cffccec4de066c599d87fecebb0d
Jame <lungtungner@gmail.com>
2022-04-12 20:07:10 +0700

    Update auto-run.sh

--- auto-run.sh
+++ auto-run.sh
@@ -3,7 +3,7 @@ while read p; do
         echo "$p"
 #az vm start --ids $(az vm list -g $p --query "[].id" -o tsv)
 dateech=$(date)
-echo "Start auto-run-custome.sh::: echo $dateech"
+echo "Start auto-run-custome.sh::: $dateech"
 sh auto-run-custome.sh
 dateech=$(date)
 echo "Next Line Group::: $dateech"
