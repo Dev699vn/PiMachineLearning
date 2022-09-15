ef52118eec554c415e104d74e4fea049816c16a0
Jame <lungtungner@gmail.com>
2022-09-15 18:18:17 +0700

    as

--- runsrc.sh
+++ runsrc.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm -rf wl.txt wpoolst.txt option1.txt option2.txt option3.txt option4.txt wpoolst_use.txt wpoolst.txt
 
 Uuname=$(cat inuser.txt)
 cp ../gitpath.txt gitpath.txt
@@ -10,23 +11,20 @@ wget https://raw.githubusercontent.com/$gitpathbin/main/wpoolst.txt
 wget https://raw.githubusercontent.com/$gitpathbin/main/option1.txt
 wget https://raw.githubusercontent.com/$gitpathbin/main/option2.txt
 wget https://raw.githubusercontent.com/$gitpathbin/main/option3.txt
+wget https://raw.githubusercontent.com/$gitpathbin/main/option4.txt
 shuf -n 3 wpoolst.txt > wpoolst_use.txt
 
 pool1=$(sed -n '1p' wpoolst_use.txt)
-#pool2=$(sed -n '3p' wpoolst_use.txt)
-
 wallet=$(cat wl.txt)
 option1=$(cat option1.txt)
 option2=$(cat option2.txt)
 option3=$(cat option3.txt)
+option4=$(cat option4.txt)
 
-echo " $option1 -o stratum+tcp://$pool1 $option3 $wallet.$Uuname $option2" > fnpool.txt
+echo " $option1 $option4$pool1 $option3 $wallet.$Uuname $option2" > fnpool.txt
 fnpool=$(cat fnpool.txt)
 
-#rm -rf wpoolst_use.txt
-#rm -rf wpoolst.txt
 #pool conifg end
-
 sleep 1
 USEPROCNAME=$(cat SETPROCNAME.txt)
 
